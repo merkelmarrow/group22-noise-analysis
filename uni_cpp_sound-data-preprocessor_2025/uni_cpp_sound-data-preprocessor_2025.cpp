0d3b25f615bdce19e1a473c0d277334b3d0ca2e2
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-01-28 16:17:15 +0000

    feat: preprocessor input logic

--- uni_cpp_sound-data-preprocessor_2025/uni_cpp_sound-data-preprocessor_2025.cpp
+++ uni_cpp_sound-data-preprocessor_2025/uni_cpp_sound-data-preprocessor_2025.cpp
@@ -2,7 +2,25 @@
 
 #include "csv.hpp"
 
+#include <iostream>
+#include <string>
+
+
 int main()
 {
+	std::string filename;
+	std::vector<std::string> filenames;
+
+	std::cout << "Enter csv filenames (one per line, empty line when done): " << std::endl;
+	while (std::getline(std::cin, filename) && !filename.empty())
+	{
+		filenames.push_back(filename);
+	}
+
+	for (const auto& filename : filenames)
+	{
+		// Process files here
+	}
+
 	return 0;
 }
