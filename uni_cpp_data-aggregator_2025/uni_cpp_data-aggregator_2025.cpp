a388c07fec4218a893697141e90563fd4b8db150
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-02-13 20:45:32 +0000

    feat: add time of day mapping and data grouping
    - grouped_data map organises records by location and name
    - aggregated_data stores all records linearly
    - populate data structures during parsing

--- uni_cpp_data-aggregator_2025/uni_cpp_data-aggregator_2025.cpp
+++ uni_cpp_data-aggregator_2025/uni_cpp_data-aggregator_2025.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <vector>
 #include <stdexcept>
+#include <utility>
 #include "csv.hpp"
 
 // helper func to remove spaces
@@ -31,8 +32,25 @@ int main()
 			{"3", "friday"},
 			{"4", "sunday"}
 		};
+		std::map<std::string, std::string> TOD_names = {
+			{"Morn", "morning"},
+			{"Noon", "afternoon"},
+			{"Afternoon", "evening"}
+		};
 
-		std::vector<std::string> header = { "Location", "Time of Day", "Day", "Average Sound Level" };
+		std::vector<std::string> header = { "location", "time_of_day", "day", "avg_gain" };
+
+		// maps key pair (ie. front_gate, morning) to a vector of records
+		// ie. one record could look like
+		// location, time_of_day, day, avg_gain
+		// pearse_st_gate, morning, friday, 73.3892
+		// pearse_st_gate, morning, friday, 63.9092
+		// etc...
+		std::map<std::pair<std::string, std::string>, std::vector<std::vector<std::string>>> grouped_data;
+
+		// just stores all the records together to create an aggregated csv
+		std::vector<std::vector<std::string>> aggregated_data;
+		aggregated_data.push_back(header);
 
 		for (csv::CSVRow& row : reader) {
 			// skip first row
@@ -40,22 +58,27 @@ int main()
 
 			for (size_t i = 0; i < row.size(); i += 5) {
 				std::string location_raw = trim(row[i].get<std::string>());
-				std::string time_of_day = trim(row[i + 1].get<std::string>());
+				std::string time_of_day_raw = trim(row[i + 1].get<std::string>());
 				std::string day_raw = trim(row[i + 2].get<std::string>());
 				std::string avg_sound = trim(row[i + 3].get<std::string>());
 
 				std::string location;
 				std::string day;
+				std::string time_of_day;
 
 				try {
 					location = location_names[location_raw];
 					day = day_names[day_raw];
+					time_of_day = TOD_names[time_of_day_raw];
 				}
 				catch (...) {
-					throw std::runtime_error("Location (A,B,C) or time of day (1,2,3) not recognised.");
+					throw std::runtime_error("Location (A,B,C) or day (1,2,3,4) not recognised.");
 				}
 
 				std::vector<std::string> record = { location, time_of_day, day, avg_sound };
+
+				grouped_data[{location, time_of_day}].push_back(record);
+				aggregated_data.push_back(record);
 			}
 
 
