734071bc3b1545228f9f7f244c90e07bfffcfce8
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-02-13 20:06:36 +0000

    feat(wip): setup csv parser and lookup maps

--- uni_cpp_data-aggregator_2025/uni_cpp_data-aggregator_2025.cpp
+++ uni_cpp_data-aggregator_2025/uni_cpp_data-aggregator_2025.cpp
@@ -1,10 +1,33 @@
-﻿// uni_cpp_data-aggregator_2025.cpp : Defines the entry point for the application.
-//
-
-using namespace std;
+﻿#include <string>
+#include <map>
+#include <vector>
+#include "csv.hpp"
 
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+	try {
+		const std::string inputFile = "group22-data.csv";
+
+		csv::CSVReader reader(inputFile);
+
+		// lookup maps for raw values to text
+		std::map<std::string, std::string> locationNames = {
+			{"A", "front_gate"},
+			{"B", "nassau_st_gate"},
+			{"C", "pearse_st_gate"}
+		};
+		std::map<std::string, std::string> dayNames = {
+			{"1", "wednesday"},
+			{"2", "thursday"},
+			{"3", "friday"},
+			{"4", "sunday"}
+		};
+
+		std::vector<std::string> header = { "location", "time_of_day", "day", "avg_gain" };
+
+	}
+	catch (...) {
+		std::cerr << "Something went wrong. Unsuccessful parse.\n";
+	}
+
 }
